b4d8d8008f086264563a68112b4b2f252867bf25
bacnghg <nhb.fit.hut@gmail.com>
2017-05-31 17:59:02 +0700

    fc

---
+++ app/src/main/java/com/example/admin/project1/NoteAddActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.project1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NoteAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_add);
+    }
+}
